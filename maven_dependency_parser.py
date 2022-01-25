5ff5e60131fb753d07efbbc9ec1b778e1863d971
Ben Lyons <bencl1998@gmail.com>
2022-01-24 21:34:57 -0500

    Add additional heuristics for dependency to cpe matching

--- maven_dependency_parser.py
+++ maven_dependency_parser.py
@@ -1,5 +1,7 @@
+import sys
 import xml.etree.ElementTree as xml
 import sqlite3
+import re
 
 def extract_dependencies(pom_file):
     dep_list = []
@@ -7,32 +9,64 @@ def extract_dependencies(pom_file):
     pom = xml.parse(pom_file)
     dependencies = pom.find('m:dependencies', ns)
     for dependency in dependencies.findall('m:dependency', ns):
-        product = dependency.find('m:artifactId', ns).text
+        groupid = dependency.find('m:groupId', ns).text
+        artifactid = dependency.find('m:artifactId', ns).text
         version = dependency.find('m:version', ns).text
-        dep_list.append((product, version))
+        dep_list.append((groupid, artifactid, version))
     return dep_list
+
+def match(groupid, artifactid, vendor, product):
+    artifactid = artifactid.replace('-', '_') # most product names use _ instead of -
+    if re.match(product, artifactid):
+        return True
+
+    if re.match(artifactid, product):
+        return True
+
+    # compare with group id, but ignore the first part because its usually com., net., gov.
+    group_parts = groupid.split('.')
+    if len(group_parts) > 1:
+        for index, part in enumerate(group_parts):
+            if index > 0 and re.match(product, part):
+                print(product, groupid)
+                return True
+
+    return False
 
 def find_vulns(dep_list, db):
     print("Found vulnerabilities:")
     cur = db.cursor()
 
-    for (product, version) in dep_list:
-        res = cur.execute("SELECT distinct cve_match.cve_id FROM cve_match where cve_match.product = ? and cve_match.version = ?", (product, version)).fetchall()
+    for (groupid, artifactid, version) in dep_list:
+        res = cur.execute("SELECT distinct cpes.vendor, cpes.product, cpes.cve_id from cpes, cve_match where cve_match.version = ? and cve_match.cve_id = cpes.cve_id and cpes.product = cve_match.product and cpes.part = 'a'", (version,)).fetchall()
 
         if not res:
             continue
 
+        cve_list = []
+        for vendor, product, cve_id in res:
+            if match(groupid, artifactid, vendor, product):
+                cve_list.append(cve_id)
+
+        if not cve_list:
+            continue
+
         print()
-        print("Dependency:", product)
+        print("Dependency:", artifactid)
         print("Version:", version)
         print("Vulnerabilities:")
 
-        for cve_id in res:
-            print("- " + cve_id[0])
+        for cve_id in cve_list:
+            print("- " + cve_id)
 
 
 def main():
-    dep_list = extract_dependencies("pom.xml")
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} pom.xml")
+        sys.exit(1)
+
+    pom_file = sys.argv[1]
+    dep_list = extract_dependencies(pom_file)
     db = sqlite3.connect("nvd.db")
     find_vulns(dep_list, db)
 
